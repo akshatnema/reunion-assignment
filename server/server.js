309b109dc50713ecb7b6bce62a215e93fb2c06ea
akshatnema <akshatnema.school@gmail.com>
2023-11-24 00:37:41 +0530

    urlencoded data format added

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 app.use(cors());
 
 app.use('/api/property', authenticatedUser, propertyRoute)
